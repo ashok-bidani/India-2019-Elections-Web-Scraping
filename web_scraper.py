f5ad16208e4ea3433e4daef72d0e9fc771a3b10d
Ashok Bidani <ashok.bidani.321@gmail.com>
2019-08-04 20:46:39 -0500

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -13,3 +13,49 @@ for i in range (0,6):
     #attributes for each course
     #note: here I will just need to 
     courseList = soup.find_all('div', attrs={'class': 'course-finder_results_item course-finder_results_item - undergraduate'})
+    
+# loop through each course
+for courseListItem in courseList:
+    
+    #get course name
+    try:
+        courseNameElement = courseListItem.find('div', attrs={'class': "col-sm-24 col-md-18 col-lg-20"})
+        courseName = courseNameElement.find('a').text
+    except:
+        courseName = ''
+            
+    # get degree name
+    try:
+        degreeTypeElement = courseListItem.find('div', attrs={'class': "course-finder_results_item_award"})
+        degreeName = degreeTypeElement.text
+    except:
+        degreeName = ''
+            
+    #get school the course is part of
+    try:
+        courseSchoolElement = courseListItem.find('div', attrs={'class': "course-finder_results_item_md course-finder_results_item_md — school"})
+        courseSchool = courseSchoolElement.find('a').text
+    except:
+        courseSchool = ''
+ 
+    #get course duration 
+    try:
+        courseDurationElement = courseListItem.find('div', attrs={'class': "course-finder_results_item_md course-finder_results_item_md — duration"})
+        courseDuration = courseDurationElement.find_all('span')[2].text
+    except:
+        courseDuration = ''
+ 
+    #get course code
+    try:
+        courseCodeElement = courseListItem.find('div', attrs={'class': "course-finder_results_item__md course-finder_results_item_md — code"})
+        courseCode = courseCodeElement.find_all('span')[2].text
+    except:
+        courseCode = ''
+            
+    print(courseName)
+    print(degreeName)
+    print(courseSchool)
+    print(courseDuration)
+    print(courseCode)
+    print (" — — — — — — -\n")
+    
