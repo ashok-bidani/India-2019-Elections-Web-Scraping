37510594c8cdc3e87b65e59093af1ef78acdc7b1
Ashok Bidani <ashok.bidani.321@gmail.com>
2019-08-15 15:01:04 -0500

    Update web_scraper.py. Now I obtain list of state codes from the initial landing page.

--- web_scraper.py
+++ web_scraper.py
@@ -1,61 +1,57 @@
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
 from bs4 import BeautifulSoup
-import urllib.request as ur
 
-#loop through pages
-for i in range (0,6):
-    
-    #read and open url to scrape
-    #note: for India election I will first need to scrape "http://results.eci.gov.in/pc/en/partywise/partywiseresult-U01.htm" for the "Select State" menu and capture all of the values in that option as this is part of the url of each state. Then I will loop through the items in this list to scrape each state detail url.
-    urlToScrape = "https://www.city.ac.uk/courses?level=Undergraduate&p=" + str(i * 10 + 1)
-    r = ur.urlopen(urlToScrape).read()
-    soup = BeautifulSoup(r, "lxml")
-    
-    #attributes for each course
-    #note: here I will just need to 
-    courseList = soup.find_all('div', attrs={'class': 'course-finder_results_item course-finder_results_item - undergraduate'})
-    
-# loop through each course
-for courseListItem in courseList:
-    
-    #get course name
-    try:
-        courseNameElement = courseListItem.find('div', attrs={'class': "col-sm-24 col-md-18 col-lg-20"})
-        courseName = courseNameElement.find('a').text
-    except:
-        courseName = ''
-            
-    # get degree name
-    try:
-        degreeTypeElement = courseListItem.find('div', attrs={'class': "course-finder_results_item_award"})
-        degreeName = degreeTypeElement.text
-    except:
-        degreeName = ''
-            
-    #get school the course is part of
-    try:
-        courseSchoolElement = courseListItem.find('div', attrs={'class': "course-finder_results_item_md course-finder_results_item_md — school"})
-        courseSchool = courseSchoolElement.find('a').text
-    except:
-        courseSchool = ''
- 
-    #get course duration 
-    try:
-        courseDurationElement = courseListItem.find('div', attrs={'class': "course-finder_results_item_md course-finder_results_item_md — duration"})
-        courseDuration = courseDurationElement.find_all('span')[2].text
-    except:
-        courseDuration = ''
- 
-    #get course code
-    try:
-        courseCodeElement = courseListItem.find('div', attrs={'class': "course-finder_results_item__md course-finder_results_item_md — code"})
-        courseCode = courseCodeElement.find_all('span')[2].text
-    except:
-        courseCode = ''
-            
-    print(courseName)
-    print(degreeName)
-    print(courseSchool)
-    print(courseDuration)
-    print(courseCode)
-    print (" — — — — — — -\n")
-    
+# This function makes an HTTP GET request to retrieve the content at "url"
+def retrieve_html(url):
+    try: 
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+    except RequestException as e:
+        log_error('Error processing request to {0}: {1}'.format(url, str(e)))
+        return None
+
+# Return True if response is HTML, False otherwise    
+def is_good_response(resp):
+    content_type = resp.headers['Content-Type'].lower()
+    return(resp.status_code == 200
+          and content_type is not None
+          and content_type.find('html') > -1)
+
+# Error-logging function which prints any errors.
+def log_error(e):
+    print(e)
+
+# The web-scraping of Indian election results will actually take several iterations because the country is broken up into states, each of which have a corresponding alphanumeric code, and then each state is broken up into constituencies, each of which is numbered. The election result URL for each constituency includes both of these, for example: "http://results.eci.gov.in/pc/en/constituencywise/ConstituencywiseU011.htm?ac=1" where U01 represents the state code and 1 is the constituency value.
+
+# So I will scrape the initial page for the state alphanumeric codes, and then scrape a page from each state to obtain the list of constituency values for that state. Finally, I will scrape each individual constituency page to obtain the data I actually want about election results (candidates, parties, and number/percentage of votes for each candidate). There should be 543 constituencies if the data is complete.
+
+# PART 1: Obtain all possible URLs to scrape data from
+
+# First I retrieve the list of state codes.
+# Pass the initial landing page for 2019 Indian Election to the "retrieve_html" function
+initial_raw_html = retrieve_html('http://results.eci.gov.in/pc/en/constituencywise/ConstituencywiseU011.htm?ac=1')
+
+# Use Beautiful Soup to narrow down text to the state codes from the initial HTML document, which is basically one large table
+# Use the "lxml" parser
+initial_html = BeautifulSoup(initial_raw_html, "lxml")
+bigtable = initial_html.find('table', class_='tabc')
+bigrow = bigtable.findAll('tr')[10]
+# State codes are included under "options" tags
+state_options = bigrow.td.findAll('option')[1:]
+
+# Create a list to hold the state codes/abbreviations
+state_abbreviations_list = []
+
+# The last (37th) "option" tag refers to something else from the states, so we exclude that one and take the option values in options up to 36. Then we sort the list.
+for option in state_options[:36]:
+    state_abbreviations_list.append(option['value'])
+state_abbreviations_list.sort()
+print(state_abbreviations_list)
+
+
+
